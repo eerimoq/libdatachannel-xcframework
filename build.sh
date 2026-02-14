8fc1f529e2d158beeff9a9a0d3ba5846b35838ae
Erik Moqvist <erik.moqvist@gmail.com>
2026-02-14 07:00:38 +0100

    Only iOS.

---
+++ build.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Copyright (c) shogo4405 and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause License found in the
+# LICENSE file in the root directory of this source tree.
+
+set -euo pipefail
+
+function clone_and_patch() {
+    if [ ! -d libdatachannel ] ; then
+      git clone git@github.com:paullouisageneau/libdatachannel.git
+      pushd libdatachannel
+      git checkout refs/tags/v0.24.0
+      git submodule update --init --recursive --depth 1
+      pushd deps/libsrtp
+      git apply ../../../libsrtp.patch
+      popd
+      popd
+    fi
+    
+    if [ ! -d OpenSSL ] ; then
+      git clone git@github.com:krzyzanowskim/OpenSSL.git
+      pushd OpenSSL
+      git checkout refs/tags/3.3.3001
+      popd
+    fi
+    
+    if [ ! -d ios-cmake ] ; then
+      git clone git@github.com:leetal/ios-cmake.git
+      pushd ios-cmake
+      git checkout refs/tags/4.5.0
+      popd
+    fi
+}
+
+function build() {
+    export OPENSSL_ROOT_DIR=$(pwd)/OpenSSL/iphoneos
+    BUILD=build/iphoneos
+    
+    cmake libdatachannel \
+      -B $BUILD \
+      -G Xcode \
+      -DCMAKE_TOOLCHAIN_FILE=../ios-cmake/ios.toolchain.cmake \
+      -DPLATFORM=OS64 \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DBUILD_SHARED_DEPS_LIBS=OFF \
+      -DNO_WEBSOCKET=YES \
+      -DNO_EXAMPLES=YES \
+      -DNO_TESTS=YES
+    cmake --build $BUILD --config Release
+    
+    libtool -static -o $BUILD/libdatachannel.a \
+      $BUILD/Release-iphoneos/libdatachannel.a \
+      $BUILD/deps/libsrtp/Release-iphoneos/libsrtp2.a \
+      $BUILD/deps/usrsctp/usrsctplib/Release-iphoneos/libusrsctp.a \
+      $BUILD/deps/libjuice/Release-iphoneos/libjuice.a \
+      $OPENSSL_ROOT_DIR/lib/*.a
+    
+    export OPENSSL_ROOT_DIR=$(pwd)/OpenSSL/iphonesimulator
+    BUILD=build/iphonesimulator
+    
+    cmake libdatachannel \
+      -B $BUILD \
+      -G Xcode \
+      -DCMAKE_TOOLCHAIN_FILE=../ios-cmake/ios.toolchain.cmake \
+      -DPLATFORM=SIMULATORARM64 \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DBUILD_SHARED_DEPS_LIBS=OFF \
+      -DNO_WEBSOCKET=YES \
+      -DNO_EXAMPLES=YES \
+      -DNO_TESTS=YES
+    cmake --build $BUILD --config Release
+    
+    libtool -static -o $BUILD/libdatachannel.a \
+      $BUILD/Release-iphonesimulator/libdatachannel.a \
+      $BUILD/deps/libsrtp/Release-iphonesimulator/libsrtp2.a \
+      $BUILD/deps/usrsctp/usrsctplib/Release-iphonesimulator/libusrsctp.a \
+      $BUILD/deps/libjuice/Release-iphonesimulator/libjuice.a \
+      $OPENSSL_ROOT_DIR/lib/*.a
+}
+
+function create_xcframework() {
+    rm -rf include
+    mkdir -p include/libdatachannel
+    cp -r libdatachannel/include/rtc include/libdatachannel
+    cp module.modulemap include/libdatachannel/module.modulemap
+    
+    rm -rf libdatachannel.xcframework
+    xcodebuild -create-xcframework  \
+        -library ./build/iphoneos/libdatachannel.a -headers include  \
+        -library ./build/iphonesimulator/libdatachannel.a -headers include  \
+        -output libdatachannel.xcframework
+    
+    zip -r libdatachannel.xcframework.zip libdatachannel.xcframework
+    swift package compute-checksum libdatachannel.xcframework.zip
+}
+
+clone_and_patch
+build
+create_xcframework
